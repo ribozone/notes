fe8b89fe96fd9ac1191c45673603dd0050958b1d
loren <151664012+ribozone@users.noreply.github.com>
2025-04-07 17:44:45 -0400

    date priority

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,7 +56,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["git", "filesystem", "frontmatter"],
+        priority: ["filesystem", "git", "frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
